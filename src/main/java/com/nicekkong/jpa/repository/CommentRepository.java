5add0903859f6d1fa99045b5bc82ddd84841034b
nicekkong <nicekkong@gmail.com>
2019-07-14 23:16:35 +0900

    스프링 데이터 Common 6. 비동기 쿼리 메소드

--- src/main/java/com/nicekkong/jpa/repository/CommentRepository.java
+++ src/main/java/com/nicekkong/jpa/repository/CommentRepository.java
@@ -5,13 +5,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.List;
+import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findByCommentContainsIgnoreCase(String keyword);
+//    List<Comment> findByCommentContainsIgnoreCase(String keyword);
 
     List<Comment> findByCommentContainsIgnoreCaseAndLikeCountGreaterThan(String keyword, int likeCount, Sort sort);
 
@@ -20,5 +23,12 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findAll(Pageable pageable);
 
     Stream<Comment> findByCommentContains(String comment, Sort sort);
+
+    // JDK 1.5 방식 , SpringApplication에 @EnableAsync 를 추가해주어야 한다.
+    @Async
+    Future<List<Comment>> findByCommentContains(String comment);
+
+    @Async
+    ListenableFuture<List<Comment>> findByCommentContainsIgnoreCase(String comment);
 
 }
--- src/test/java/com/nicekkong/jpa/CommentRepositoryTest.java
+++ src/test/java/com/nicekkong/jpa/CommentRepositoryTest.java
@@ -7,16 +7,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeoutException;
 
 
 @RunWith(SpringRunner.class)
@@ -27,102 +25,51 @@ public class CommentRepositoryTest {
     private CommentRepository commentRepository;
 
     @Test
-    public void commentRepoTest() {
+    public void asyncFutureTest() throws ExecutionException, InterruptedException, TimeoutException {
 
-        Comment comment = new Comment();
-        comment.setComment("Spring Data JPA");
-        comment.setLikeCount(1);
+        this.createComment("Spring Data", 100);
+        this.createComment("JPA for Spring", 200);
 
-        commentRepository.save(comment);
+        // Future : JDK 1.5부터 사용됨
+        Future<List<Comment>> future = commentRepository.findByCommentContains("Spring");
+        System.out.println("============================================");
+        System.out.println("is Done? " + future.isDone());
 
-        List<Comment> comments = commentRepository.findByCommentContainsIgnoreCase("spring");
 
-        assertThat(comments.size()).isEqualTo(1);
-
-    }
-
-    @Test
-    public void commentQueryTest2() {
-
-        Comment comment = new Comment();
-        comment.setComment("Spring Data JPA");
-        comment.setLikeCount(20);
-
-        commentRepository.save(comment);
-
-        List<Comment> comments = commentRepository.findByCommentContainsIgnoreCaseAndLikeCountGreaterThan("spring", 10,
-                Sort.by(Sort.Direction.DESC, "likeCount", "comment"));
-
-        assertThat(comments.size()).isEqualTo(1);
-
+        List<Comment> comments = future.get();
+//        List<Comment> comments = future.get(3, TimeUnit.SECONDS);
+        comments.forEach(c -> System.out.println(c.getComment()));
     }
 
 
     @Test
-    public void commentQueryTest2_2() {
+    public void asyncListenableFutureTest() throws ExecutionException, InterruptedException, TimeoutException {
 
-        Comment comment = new Comment();
-        comment.setComment("Spring Data JPA");
-        comment.setLikeCount(20);
+        this.createComment("Spring Data", 100);
+        this.createComment("JPA for Spring", 200);
 
-        commentRepository.save(comment);
+        // Future : JDK 1.5부터 사용됨
+        ListenableFuture<List<Comment>> future = commentRepository.findByCommentContainsIgnoreCase("Spring");
+        System.out.println("============================================");
+        System.out.println("is Done? " + future.isDone());
 
-        List<Comment> comments = commentRepository.findByCommentContainsIgnoreCaseAndLikeCountGreaterThan("spring", 10,
-                Sort.by(Sort.Order.desc("likeCount"), Sort.Order.asc("comment")));
+        future.addCallback(new ListenableFutureCallback<List<Comment>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println(throwable);
+            }
 
-        assertThat(comments.size()).isEqualTo(1);
-
+            @Override
+            public void onSuccess(List<Comment> comments) {
+                comments.forEach(c -> System.out.println(c.getComment()));
+            }
+        });
     }
 
 
-    @Test
-    public void commentQueryTest3() {
 
-        createComment("Spring JPA", 1000);
-        createComment("Spring JPA Study", 500);
 
-        List<Comment> comments = commentRepository.findByCommentContainsIgnoreCaseAndLikeCountGreaterThanOrderByLikeCountDesc("spring", 10);
 
-        assertThat(comments.size()).isEqualTo(2);
-        assertThat(comments).first().hasFieldOrPropertyWithValue("likeCount", 1000);
-        assertThat(comments).first().hasFieldOrPropertyWithValue("comment", "Spring JPA");
-//        assertThat(comments.get(0).getComment()).isEqualTo("Spring");
-
-    }
-
-    @Test
-    public void commentQueryTest4() {
-
-        IntStream.range(0, 54).forEach(n ->
-           createComment("Comment!!! " + n, n * 5)
-        );
-
-        Page<Comment> comments = commentRepository.findAll(PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "likeCount")));
-
-        System.err.println("=START===============================");
-        System.out.println(comments.getTotalPages());
-        comments.getContent().forEach(c -> System.out.println(c.getComment()));
-        System.err.println("==END================================");
-    }
-
-    @Test
-    public void commentQueryTest5() {
-
-        IntStream.range(1, 54).forEach(n ->
-                createComment("Comment!!! " + n, n * 5)
-        );
-
-        PageRequest page = PageRequest.of(1, 10, Sort.by(Sort.Order.desc("likeCount")));
-
-        Sort sort = Sort.by(Sort.Order.desc("likeCount"), Sort.Order.asc("comment"));
-
-        try(Stream<Comment> commentStream = commentRepository.findByCommentContains("Comment", sort)) {
-            Comment c1 = commentStream.findFirst().get();
-            System.out.println("======>" + c1.getLikeCount());
-            assertThat(c1.getLikeCount()).isEqualTo(265);
-        }
-    }
-    
     private Comment createComment(String commentContent, int likeCount) {
 
         Comment comment = new Comment();
